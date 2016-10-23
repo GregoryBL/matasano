da382312db1ca82f6874cadf5608cff49781c02d
Gregory Berns-Leone <gberns.l@gmail.com>
2016-10-22 20:25:54 -0500

    Add partial solution

--- 1-set/set1_6.rb
+++ 1-set/set1_6.rb
@@ -1,9 +1,13 @@
 require_relative 'set1'
+require_relative 'text'
 # Solve 6
 def decrypt_repeating_xor(string)
-
+  bytes = base64_to_raw(string)
+  p keysize = find_key_size(bytes)
+  p sorted = bytes.group_by.with_index { |_, ind| ind % keysize }.map { |a| a[1] }
+  p key = sorted.map { |bts| decode_single_xor(raw_to_hex(bts))[:byte].chr }.join('')
+  repeating_key_xor(base64_to_raw(string).map{|a| a.chr}, key)
 end
-
 
 ## Helpers
 def hamming_distance(str1, str2)
@@ -27,8 +31,14 @@ def hamming_distance(str1, str2)
   end
 end
 
-def find_key_size(string)
-
+def find_key_size(bytes)
+  (2..40).min_by do |size|
+    c1 = bytes[0..size].map { |n| n.chr }.join('')
+    c2 = bytes[size..2*size].map { |n| n.chr }.join('')
+    c3 = bytes[2*size..3*size].map { |n| n.chr }.join('')
+    c4 = bytes[3*size..4*size].map { |n| n.chr }.join('')
+    p (hamming_distance(c1, c2) + hamming_distance(c1, c3) + hamming_distance(c1, c4)) / size.to_f
+  end
 end
 
 def base64_to_raw(base64string)
@@ -46,3 +56,6 @@ def base64_4_to_raw(base64_array)
   v3 = value - v1 * 2 ** 16 - v2 * 2 ** 8
   [v1, v2, v3]
 end
+
+p ct = hex_to_base_64(repeating_key_xor("This code is going to turn out to be surprisingly useful later on. Breaking repeating-key XOR (Vigenere) statistically is obviously an academic exercise, a Crypto 101 thing. But more people know how to break it than can actually break it, and a similar technique breaks something much more important.", "his"))
+p decrypt_repeating_xor(ct)
